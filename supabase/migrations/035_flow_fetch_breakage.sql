c9c550469ff5936b21b85031cf5a80f631f981c7
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2026-07-01 12:03:14 +0530

    Fix constraint name in migration 035

--- supabase/migrations/035_flow_fetch_breakage.sql
+++ supabase/migrations/035_flow_fetch_breakage.sql
@@ -1,8 +1,9 @@
 BEGIN;
 
+ALTER TABLE flow_nodes DROP CONSTRAINT IF EXISTS flow_nodes_node_type_check;
 ALTER TABLE flow_nodes DROP CONSTRAINT IF EXISTS flow_nodes_type_check;
 
-ALTER TABLE flow_nodes ADD CONSTRAINT flow_nodes_type_check
+ALTER TABLE flow_nodes ADD CONSTRAINT flow_nodes_node_type_check
 CHECK (
   node_type IN (
     'start',
